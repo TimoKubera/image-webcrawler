ec5bae5f8fd5aedcb020ba11727848a07bbf9998
Timo <timokubera@yahoo.com>
2020-04-15 00:44:20 +0200

    Added download funtionality.

--- imgcrawler.py
+++ imgcrawler.py
@@ -1,7 +1,10 @@
 from selenium import webdriver;
 from selenium.webdriver import ChromeOptions;
 from selenium.webdriver import ActionChains;
+import os;
 import sys;
+import pip._vendor.requests;
+import shutil;
 
 MIN_ARG_LEN = 2;
 FLAG = 1;
@@ -9,28 +12,47 @@ FLAG = 1;
 # The path needs to be updated according to your installation path of the chromedriver.
 chromedriver = "/usr/local/bin/chromedriver";
 opts = ChromeOptions();
-opts.headless = False;
+opts.headless = True;
 driver = webdriver.Chrome(executable_path = chromedriver, options = opts);
 
 # Searches for and prints the images.
 dl = False;
 
-def crawl(url):
-    print("crawling " + sys.argv[url] + " ...");
-    driver.get(str(sys.argv[url]));
+def crawl(i):
+    print("crawling " + sys.argv[i] + " ...");
+    driver.get(sys.argv[i]);
     images = driver.find_elements_by_tag_name("img");
+    # j counts the img elements
+    j = 0;
     for ele in images:
-        print(ele.get_attribute("src"));
-        # To do: Optionally download the images.
+        if ele.get_attribute("src"):
+            print(ele.get_attribute("src"));
+        else:
+            continue;
+        # Optionally download the images.
         if dl == True:
-            ActionChains(driver).context_click(ele).perform();
+            download(ele.get_attribute("src"), i, j);
             # In the next step, 'save image as' needs to be selected,
             # which does not work using selenium.
+            j += 1;
     print("");
+
+# Downloads the images
+def download(url, i , j):
+    print("Downloading " + str(url) + " ...");
+    response = pip._vendor.requests.get(url, stream = True);
+    save_image(response, i, j);
+    del response;
+
+# Saves the images in the local downloads folder.
+def save_image(img, i, j):
+    with open((os.getcwd() + "/downloads/url_{i}_img_{j}").format(i = i - MIN_ARG_LEN, j = j), "wb") as out_file:
+        shutil.copyfileobj(img.raw, out_file);
+
 
 if len(sys.argv) < MIN_ARG_LEN:
     print("Please enter at least one valid URL!");
-# To do: Optionally download the images.
+# Optionally download the images.
 elif sys.argv[FLAG] == "-dl":
     dl = True;
     if len(sys.argv) < MIN_ARG_LEN + FLAG:
